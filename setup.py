9628422cdda7bd91e6db34720256851b9b7c2f18
Jakub Gocławski <jakub@goclawski.net>
2016-10-31 11:19:57 +0100

    Add specific version of supported Python to setup.py.

--- setup.py
+++ setup.py
@@ -27,5 +27,6 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
     ]
 )
