467888cfcd2a0c7014f0150a272e905a0991ba42
Jakub Skiepko <jakub.skiepko@socialwifi.com>
2017-01-04 12:18:57 +0100

    Refactor.

--- kubepy/definition_manager.py
+++ kubepy/definition_manager.py
@@ -1,19 +1,26 @@
+import collections
 import pathlib
 
 import yaml
 
 
-class DefinitionManager:
+class DefinitionManager(collections.Mapping):
     def __init__(self, directory: pathlib.Path):
         self.directory = directory
 
-    def get_definition(self, name):
-        path = self.directory / (name + '.yml')
+    def __getitem__(self, name):
+        path = self.get_path_from_name(name)
         return self.get_yaml_from_file(path)
 
-    def get_sorted_definitions(self):
-        for path in sorted(self.directory.iterdir()):
-            yield self.get_yaml_from_file(path)
+    def __iter__(self):
+        for path in sorted(self.directory.glob('*.yml')):
+            yield path.name[:-4]
+
+    def __len__(self):
+        return len(list(self))
+
+    def get_path_from_name(self, name):
+        return self.directory / (name + '.yml')
 
     def get_yaml_from_file(self, path):
         with path.open() as yaml_file:
